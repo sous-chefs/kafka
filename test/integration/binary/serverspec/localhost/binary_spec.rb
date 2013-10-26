dabd8fa2e35feac1710fd55967c63f7307d11e59
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-10-26 12:32:56 +0200

    Test the right things in binary integration specs
    Suppose I just copied the source integration specs
    and then just didn't give a fuck. Oh well.

--- test/integration/binary/serverspec/localhost/binary_spec.rb
+++ test/integration/binary/serverspec/localhost/binary_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe 'kafka::binary' do
-  describe file('/opt/kafka/build') do
+  describe file('/opt/kafka/dist') do
     it { should be_a_directory }
     it { should be_mode 755 }
     it { should be_owned_by('kafka') }
@@ -13,10 +13,10 @@ describe 'kafka::binary' do
   describe file('/tmp/kitchen-chef-solo/cache/kafka_2.8.0-0.8.0-beta1.tgz') do
     it { should be_a_file }
     it { should be_mode 644 }
-    it { should match_md5checksum 'b90e355ea2bc1e5f62db1836fdd77502' }
+    it { should match_md5checksum 'f12e7698aff37a0e014cb9dc087f0b8f' }
   end
 
-  describe file('/opt/kafka/kafka_2.9.2-0.8.0-beta1.jar') do
+  describe file('/opt/kafka/kafka_2.8.0-0.8.0-beta1.jar') do
     it { should be_a_file }
     it { should be_owned_by('kafka') }
     it { should be_grouped_into('kafka') }
