793281a1c0b0e83f063eec6e7d070dba0ca94e9b
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-12-21 20:39:06 +0100

    Add integration specs for init.d script

--- test/integration/source/serverspec/localhost/source_spec.rb
+++ test/integration/source/serverspec/localhost/source_spec.rb
@@ -21,4 +21,80 @@ describe 'kafka::source' do
     it { should be_owned_by('kafka') }
     it { should be_grouped_into('kafka') }
   end
+
+  describe 'service kafka start' do
+    context 'when kafka is already running' do
+      before do
+        backend.run_command 'service kafka start'
+      end
+
+      it 'is actually running' do
+        expect(service 'kafka').to be_running
+      end
+
+      describe command 'service kafka start' do
+        it { should return_exit_status 0 }
+        it { should return_stdout /kafka .+ already running/ }
+      end
+    end
+
+    context 'when kafka is not already running' do
+      before do
+        backend.run_command 'service kafka stop'
+      end
+
+      it 'is not currently running' do
+        expect(service 'kafka').not_to be_running
+      end
+
+      describe command 'service kafka start' do
+        it { should return_stdout /Starting kafka/ }
+        it { should return_exit_status 0 }
+
+        it 'actually starts kafka' do
+          backend.run_command 'service kafka start'
+
+          expect(service 'kafka').to be_running
+        end
+      end
+    end
+  end
+
+  describe 'service kafka stop' do
+    context 'when kafka is running' do
+      before do
+        backend.run_command 'service kafka start'
+      end
+
+      it 'is actually running' do
+        expect(service 'kafka').to be_running
+      end
+
+      describe command 'service kafka stop' do
+        it { should return_exit_status 0 }
+        it { should return_stdout /Stopping kafka/ }
+
+        it 'actually stops kafka' do
+          backend.run_command 'service kafka stop'
+
+          expect(service 'kafka').not_to be_running
+        end
+      end
+    end
+
+    context 'when kafka is not running' do
+      before do
+        backend.run_command 'service kafka stop'
+      end
+
+      it 'is not currently running' do
+        expect(service 'kafka').not_to be_running
+      end
+
+      describe command 'service kafka stop' do
+        it { should return_stdout /Stopping kafka/ }
+        it { should return_exit_status 0 }
+      end
+    end
+  end
 end
