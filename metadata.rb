43adc5aebdc0a9b8e6e34f159f8c100f9c5dd158
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-12-22 21:14:44 +0100

    Update recipe definitions in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -8,10 +8,11 @@ description      'Installs and configures a Kafka broker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-recipe 'kafka::configure', 'Setups up Kafka environment with directories and configuration files'
+recipe 'kafka::setup', 'Setups up Kafka environment with necessary directories'
+recipe 'kafka::configure', 'Creates configuration files and init.d script'
 recipe 'kafka::source', 'Downloads, compiles and installs Kafka from source releases'
 recipe 'kafka::binary', 'Downloads, extracts and installs Kafka from binary releases'
-recipe 'kafka::standalone', 'Setups standalone ZooKeeper instance using the ZooKeeper version that is bundled with Kafka'
+recipe 'kafka::zookeeper', 'Setups standalone ZooKeeper instance using the ZooKeeper version that is bundled with Kafka'
 
 recommends 'java', '~> 1.15.4'
 
