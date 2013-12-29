c3a8792f6faba64b6789cafb68dada56feb2e372
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-12-29 02:24:10 +0100

    Remove private recipes from metadata.rb
    [ci skip]

--- metadata.rb
+++ metadata.rb
@@ -8,8 +8,6 @@ description      'Installs and configures a Kafka broker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-recipe 'kafka::setup', 'Setups up Kafka environment with necessary directories'
-recipe 'kafka::configure', 'Creates configuration files and init.d script'
 recipe 'kafka::source', 'Downloads, compiles and installs Kafka from source releases'
 recipe 'kafka::binary', 'Downloads, extracts and installs Kafka from binary releases'
 recipe 'kafka::zookeeper', 'Setups standalone ZooKeeper instance using the ZooKeeper version that is bundled with Kafka'
