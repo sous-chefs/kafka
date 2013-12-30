c0583dc0673f2ce4713e4f53251dd5dd7a33dec3
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-12-31 00:24:00 +0100

    Move all default attributes to default attributes file
    Except the ones related to the zookeeper recipe.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,7 @@ default[:kafka][:base_url] = 'https://dist.apache.org/repos/dist/release/kafka'
 default[:kafka][:checksum] = nil
 default[:kafka][:md5_checksum] = nil
 default[:kafka][:scala_version] = nil
+default[:kafka][:install_method] = :binary
 
 default[:kafka][:install_dir] = '/opt/kafka'
 default[:kafka][:config_dir] = File.join(node[:kafka][:install_dir], 'config')
@@ -22,4 +23,33 @@ default[:kafka][:group] = 'kafka'
 default[:kafka][:log_level] = 'INFO'
 default[:kafka][:jmx_port] = 9999
 
-default[:kafka][:install_method] = :binary
+default[:kafka][:heap_opts] = '-Xmx1G -Xms1G'
+default[:kafka][:generic_opts] = nil
+
+default[:kafka][:broker_id] = node[:ipaddress].gsub('.', '')
+default[:kafka][:host_name] = node[:hostname]
+default[:kafka][:port] = 9092
+default[:kafka][:network_threads] = 2
+default[:kafka][:io_threads] = 2
+default[:kafka][:num_partitions] = 1
+
+default[:kafka][:socket][:send_buffer_bytes] = 1048576
+default[:kafka][:socket][:receive_buffer_bytes] = 1048576
+default[:kafka][:socket][:request_max_bytes] = 104857600
+
+default[:kafka][:log][:dirs] = '/tmp/kafka-logs'
+default[:kafka][:log][:flush_interval_messages] = 10000
+default[:kafka][:log][:flush_interval_ms] = 1000
+default[:kafka][:log][:retention_hours] = 168
+default[:kafka][:log][:retention_bytes] = 1073741824
+default[:kafka][:log][:segment_bytes] = 536870912
+default[:kafka][:log][:cleanup_interval_mins] = 1
+
+default[:kafka][:zookeeper][:connect] = []
+default[:kafka][:zookeeper][:timeout] = 1000000
+
+default[:kafka][:metrics][:polling_interval] = 5
+default[:kafka][:metrics][:reporters] = ['kafka.metrics.KafkaCSVMetricsReporter']
+
+default[:kafka][:csv_metrics][:dir] = '/tmp/kafka_metrics'
+default[:kafka][:csv_metrics][:reporter_enabled] = false
