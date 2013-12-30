16447a0d8d2d314270b8ca2a84da7553f2cf5360
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-12-30 03:03:09 +0100

    Rework (default) init.d script, again
    * Store environment variables in /etc/sysconfig/kafka. Might be blasphemy to use 'export' in these files, but I haven't seen anything about it though.
    * Source /etc/sysconfig/kafka in init.d script.
    * Try to stop kafka by sending a SIGINT and then TERM&KILL. The stop script that is bundled with kafka uses SIGINT, so why not.

--- recipes/_configure.rb
+++ recipes/_configure.rb
@@ -4,35 +4,42 @@
 #
 
 template File.join(node[:kafka][:config_dir], node[:kafka][:log4j_config]) do
-  source  'log4j.properties.erb'
-  owner node[:kafka][:user]
-  group node[:kafka][:group]
-  mode  '644'
-  variables(
+  source 'log4j.properties.erb'
+  owner  node[:kafka][:user]
+  group  node[:kafka][:group]
+  mode   '644'
+  variables({
     process: 'kafka',
     log_dir: node[:kafka][:log_dir]
-  )
+  })
 end
 
 template File.join(node[:kafka][:config_dir], node[:kafka][:config]) do
-  source  'server.properties.erb'
-  owner node[:kafka][:user]
-  group node[:kafka][:group]
-  mode  '644'
+  source 'server.properties.erb'
+  owner  node[:kafka][:user]
+  group  node[:kafka][:group]
+  mode   '644'
+end
+
+template '/etc/sysconfig/kafka' do
+  source 'kafka.env.erb'
+  owner  'root'
+  group  'root'
+  mode   '644'
+  variables({
+    main_class:   'kafkaServer kafka.Kafka',
+    jmx_port:     node[:kafka][:jmx_port],
+    config:       node[:kafka][:config],
+    log4j_config: 'log4j.properties'
+  })
 end
 
 template '/etc/init.d/kafka' do
-  source 'initd.script.erb'
-  owner 'root'
-  group 'root'
-  mode '755'
-  variables(
-    daemon_name:   'kafka',
-    main_class:    'kafkaServer kafka.Kafka',
-    jmx_port:      node[:kafka][:jmx_port],
-    config:        node[:kafka][:config],
-    log4j_config:  'log4j.properties'
-  )
+  source 'initd.sh.erb'
+  owner  'root'
+  group  'root'
+  mode   '755'
+  variables({daemon_name: 'kafka'})
 end
 
 service 'kafka' do
