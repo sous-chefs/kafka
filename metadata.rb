09c97bafe07265a5a497bebbbf3d1f9d7cd4a502
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-01-02 00:17:08 +0100

    Bump recommended version of java cookbook

--- metadata.rb
+++ metadata.rb
@@ -12,6 +12,6 @@ recipe 'kafka::source', 'Downloads, compiles and installs Kafka from source rele
 recipe 'kafka::binary', 'Downloads, extracts and installs Kafka from binary releases'
 recipe 'kafka::zookeeper', 'Setups standalone ZooKeeper instance using the ZooKeeper version that is bundled with Kafka'
 
-recommends 'java', '~> 1.15.4'
+recommends 'java', '~> 1.17'
 
 supports 'centos'
