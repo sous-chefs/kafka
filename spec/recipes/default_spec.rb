29eea33dc341744ddd8381fa991f884e63c6a017
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-01-01 19:32:27 +0100

    Refactor default spec

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -3,33 +3,39 @@
 require 'spec_helper'
 
 describe 'kafka::default' do
+  let :chef_run do
+    ChefSpec::Runner.new do |node|
+      node.set[:kafka][:install_method] = install_method
+    end
+  end
+
   context 'when node[:kafka][:install_method] equals :source' do
-    let :chef_run do
-      ChefSpec::Runner.new do |node|
-        node.set[:kafka][:install_method] = :source
-      end.converge(described_recipe)
+    let :install_method do
+      :source
     end
 
     it 'includes kafka::source recipe' do
+      chef_run.converge(described_recipe)
+
       expect(chef_run).to include_recipe('kafka::source')
     end
   end
 
   context 'when node[:kafka][:install_method] equals :binary' do
-    let :chef_run do
-      ChefSpec::Runner.new.converge(described_recipe)
+    let :install_method do
+      :binary
     end
 
     it 'includes kafka::binary recipe' do
+      chef_run.converge(described_recipe)
+
       expect(chef_run).to include_recipe('kafka::binary')
     end
   end
 
   context 'when node[:kafka][:install_method] is something else' do
-    let :chef_run do
-      ChefSpec::Runner.new do |node|
-        node.set[:kafka][:install_method] = :bork
-      end
+    let :install_method do
+      :bork
     end
 
     it 'terminates the chef run' do
