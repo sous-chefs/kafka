63a811e7bfd5feab7516ca82a70ae3fab25547b7
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-01-03 20:21:06 +0100

    Remove init file requirement from configuration integration test

--- test/integration/configuration/serverspec/localhost/configure_spec.rb
+++ test/integration/configuration/serverspec/localhost/configure_spec.rb
@@ -17,15 +17,7 @@ describe 'kafka::configure' do
     it { should be_mode 644 }
   end
 
-  describe file('/etc/init.d/kafka') do
-    it { should be_a_file }
-    it { should be_owned_by('root') }
-    it { should be_grouped_into('root') }
-    it { should be_mode 755 }
-  end
-
   describe service('kafka') do
-    let(:path) { '/sbin:/usr/sbin' }
     it { should be_enabled }
   end
 end
