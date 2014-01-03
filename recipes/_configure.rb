dee17d426350cb22fdd11daf4a7282ead55fc380
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-01-03 15:11:03 +0100

    Make port in upstart script a variable instead of attribute

--- recipes/_configure.rb
+++ recipes/_configure.rb
@@ -57,7 +57,7 @@ template init_script_path do
   owner  'root'
   group  'root'
   mode   init_script_permissions
-  variables({daemon_name: 'kafka'})
+  variables({daemon_name: 'kafka', port: node[:kafka][:port]})
 end
 
 service 'kafka' do
