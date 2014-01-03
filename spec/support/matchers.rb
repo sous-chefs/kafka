25d98fc0a61f4e27f1769572eb3affb9a4b85ea7
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-01-03 17:00:29 +0100

    Update have_configured matcher
    * Use Regexp.quote for @value as well.
    * Explicitly create a RenderFileMatcher.
    * Better failure message.
    * Force @value to a String.

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -2,16 +2,17 @@
 
 RSpec::Matchers.define :have_configured do |configuration_file|
   match do |chef_run|
-    regexp = Regexp.new("^#{Regexp.quote(@attribute)}=#{@value}$")
-    render_file(configuration_file).with_content(regexp).matches?(chef_run)
+    regexp = Regexp.new("^#{Regexp.quote(@attribute)}=#{Regexp.quote(@value)}$")
+    @matcher = ChefSpec::Matchers::RenderFileMatcher.new(configuration_file)
+    @matcher.with_content(regexp).matches?(chef_run)
   end
 
   failure_message_for_should do |actual|
-    "expected that #{configuration_file} would be configured with #{@attribute} as #{@value}"
+    @matcher.failure_message_for_should
   end
 
   chain :as do |value|
-    @value = value
+    @value = value.to_s
   end
 
   chain :with do |attribute|
