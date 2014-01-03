feda706c9c32fcaa92a22bb8ddcd2f01edccd609
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-01-03 12:40:41 +0100

    Check additional directories in binary suite

--- test/integration/binary/serverspec/localhost/binary_spec.rb
+++ test/integration/binary/serverspec/localhost/binary_spec.rb
@@ -3,13 +3,6 @@
 require 'spec_helper'
 
 describe 'kafka::binary' do
-  describe file('/opt/kafka/dist') do
-    it { should be_a_directory }
-    it { should be_mode 755 }
-    it { should be_owned_by('kafka') }
-    it { should be_grouped_into('kafka') }
-  end
-
   describe file('/tmp/kitchen/cache/kafka_2.8.0-0.8.0.tar.gz') do
     it { should be_a_file }
     it { should be_mode 644 }
@@ -20,5 +13,14 @@ describe 'kafka::binary' do
     it { should be_a_file }
     it { should be_owned_by('kafka') }
     it { should be_grouped_into('kafka') }
+  end
+
+  %w(dist libs bin).each do |dir| do
+    describe file(dir) do
+      it { should be_a_directory }
+      it { should be_mode 755 }
+      it { should be_owned_by 'kafka' }
+      it { should be_grouped_into 'kafka' }
+    end
   end
 end
