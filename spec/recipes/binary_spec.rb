3659a806c225981d83493ace3a23188d27305194
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-03-16 17:18:25 +0100

    Add some funky tests to achieve 100% coverage
    I think it'd be possible to add some tests for `kafka_download` and
    `kafka_install` LWRPs and then get rid of these tests, but right now
    I'm not 100% sure.

--- spec/recipes/binary_spec.rb
+++ spec/recipes/binary_spec.rb
@@ -14,6 +14,21 @@ describe 'kafka::binary' do
       md5_checksum: '593e0cf966e6b8cd1bbff5bff713c4b3',
       mode: '644'
     })
+
+    expect(chef_run).to create_remote_file("#{Chef::Config[:file_cache_path]}/kafka_2.8.0-0.8.0.tar.gz").with({
+      source: 'https://dist.apache.org/repos/dist/release/kafka/0.8.0/kafka_2.8.0-0.8.0.tar.gz',
+      checksum: 'ecadd6cf9f59e22444af5888c8b9595c5652ffab597db038418e85dfa834062e',
+      mode: '644'
+    })
+  end
+
+  it 'validates download' do
+    # Keep this around to achieve 100% coverage until the next version of ChefSpec
+    # is released. See https://github.com/sethvargo/chefspec/issues/379
+    expect(chef_run).not_to run_ruby_block('validate-download')
+
+    remote_file = chef_run.remote_file("#{Chef::Config[:file_cache_path]}/kafka_2.8.0-0.8.0.tar.gz")
+    expect(remote_file).to notify('ruby_block[validate-download]').immediately
   end
 
   it 'extracts downloaded Kafka archive' do
@@ -26,5 +41,7 @@ describe 'kafka::binary' do
 
   it 'installs extracted Kafka archive' do
     expect(chef_run).to run_kafka_install('/opt/kafka')
+
+    expect(chef_run).to run_execute('install-kafka')
   end
 end
--- spec/recipes/source_spec.rb
+++ spec/recipes/source_spec.rb
@@ -14,6 +14,21 @@ describe 'kafka::source' do
       md5_checksum: '46b3e65e38f1bde4b6251ea131d905f4',
       mode: '644'
     })
+
+    expect(chef_run).to create_remote_file("#{Chef::Config[:file_cache_path]}/kafka-0.8.0-src.tgz").with({
+      source: 'https://dist.apache.org/repos/dist/release/kafka/0.8.0/kafka-0.8.0-src.tgz',
+      checksum: 'f4b7229671aba98dba9a882244cb597aab8a9018631575d28e119725a01cfc9a',
+      mode: '644'
+    })
+  end
+
+  it 'validates download' do
+    # Keep this around to achieve 100% coverage until the next version of ChefSpec
+    # is released. See https://github.com/sethvargo/chefspec/issues/379
+    expect(chef_run).not_to run_ruby_block('validate-download')
+
+    remote_file = chef_run.remote_file("#{Chef::Config[:file_cache_path]}/kafka-0.8.0-src.tgz")
+    expect(remote_file).to notify('ruby_block[validate-download]').immediately
   end
 
   it 'compiles Kafka source' do
@@ -22,5 +37,7 @@ describe 'kafka::source' do
 
   it 'installs compiled Kafka source' do
     expect(chef_run).to run_kafka_install('/opt/kafka')
+
+    expect(chef_run).to run_execute('install-kafka')
   end
 end
