7b06a1145d9f897516f8711070edd2e5bd557819
Brian Smith <bsmith@swig505.com>
2014-11-07 15:08:09 -0800

    Do not require host.name in server.properties
    Kafka uses this attribute to determine which address to bind, and
    you need to be able to omit host.name to bind to all interfaces.

--- recipes/_defaults.rb
+++ recipes/_defaults.rb
@@ -10,7 +10,3 @@ end
 unless broker_attribute?(:port)
   node.default.kafka.broker.port = 6667
 end
-
-unless broker_attribute?(:host, :name)
-  node.default.kafka.broker.host_name = node.hostname
-end
