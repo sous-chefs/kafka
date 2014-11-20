dc70f9da7443620555ef60ee5ebe7d99e8d8f5aa
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-11-20 15:56:16 +0100

    Extract variables in `_install` recipe to helpers

--- recipes/_install.rb
+++ recipes/_install.rb
@@ -3,10 +3,7 @@
 # Recipe:: _install
 #
 
-kafka_tar_gz = [kafka_base, kafka_archive_ext].join('.')
-local_file_path = ::File.join(Chef::Config.file_cache_path, kafka_tar_gz)
-
-kafka_download local_file_path do
+kafka_download kafka_local_download_path do
   source kafka_download_uri(kafka_tar_gz)
   checksum node.kafka.checksum
   md5_checksum node.kafka.md5_checksum
@@ -16,7 +13,7 @@ end
 execute 'extract-kafka' do
   cwd node.kafka.build_dir
   command <<-EOH.gsub(/^\s+/, '')
-    tar zxf #{local_file_path} && \
+    tar zxf #{kafka_local_download_path} && \
     chown -R #{node.kafka.user}:#{node.kafka.group} #{node.kafka.build_dir}
   EOH
   not_if { kafka_installed? }
