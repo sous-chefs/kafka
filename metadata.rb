5cd3511868a6c140510576de7f73094cb2b1a237
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-11-20 17:56:01 +0100

    Remove source mention in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -8,8 +8,7 @@ description      'Installs and configures a Kafka broker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.6.0'
 
-recipe 'kafka::source', 'Downloads, compiles and installs Kafka from source releases'
-recipe 'kafka::binary', 'Downloads, extracts and installs Kafka from binary releases'
+recipe 'kafka::default', 'Downloads, compiles and installs Kafka from binary releases'
 
 suggests 'java', '~> 1.22'
 
