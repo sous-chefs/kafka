5b2871bbb0b0131852476c94fdcc4842f4c136cd
Mathias Söderberg <mathias.soederberg@gmail.com>
2015-07-19 14:37:24 +0200

    Version bump to v0.7.2

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'mths@sdrbrg.se'
 license          'Apache 2.0'
 description      'Installs and configures a Kafka broker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.1'
+version          '0.7.2'
 
 recipe 'kafka::default', 'Downloads and installs Kafka from binary releases'
 
