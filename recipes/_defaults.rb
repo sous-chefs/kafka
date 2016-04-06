a9855b29f80e8343bfb7cd025f8930baacdcadd1
Mathias Söderberg <mathias.soederberg@gmail.com>
2016-04-06 21:39:31 +0200

    Remove default values for `broker.id` and `port`

--- recipes/_defaults.rb
+++ recipes/_defaults.rb
@@ -3,16 +3,6 @@
 # Recipe:: _defaults
 #
 
-unless broker_attribute?(:broker, :id)
-  Chef::Log.warn('Default value for `broker.id` is deprecated and will be removed in the next major version')
-  node.default['kafka']['broker']['broker_id'] = node['ipaddress'].delete('.').to_i % 2**31
-end
-
-unless broker_attribute?(:port)
-  Chef::Log.warn('Default value for `port` is deprecated and will be removed in the next major version')
-  node.default['kafka']['broker']['port'] = 6667
-end
-
 unless node['kafka']['gc_log_opts']
   node.default['kafka']['gc_log_opts'] = %W[
     -Xloggc:#{::File.join(node['kafka']['log_dir'], 'kafka-gc.log')}
