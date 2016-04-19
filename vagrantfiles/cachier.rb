5c99d11e254b96e9b31e9580ee933ce3138d6b82
Mathias Söderberg <mathias.soederberg@gmail.com>
2016-04-19 14:08:21 +0200

    Update platforms & custom settings for test-kitchen
    The old ones have been removed from Atlas. I also updated Virtualbox the
    other day and started having massive problems with creating boxes due
    to the rather custom `customize` settings. Don't really know exactly
    which ones caused it, but everything seems to be humming along nicely
    without any of them (even faster than before :)).
    
    The addition to `vagrantfiles/cachier.rb` is due to some error message
    I saw when investigating why issues with creating boxes, and it seems
    to be a bit faster with this. I have no idea what I'm doing.

--- vagrantfiles/cachier.rb
+++ vagrantfiles/cachier.rb
@@ -3,4 +3,11 @@ Vagrant.configure('2') do |c|
     c.cache.auto_detect = true
     c.cache.scope = :box
   end
+  c.vm.provider 'virtualbox' do |v|
+    v.customize [
+      'storagectl', :id,
+      '--name', 'SATA Controller',
+      '--hostiocache', 'on'
+    ]
+  end
 end
