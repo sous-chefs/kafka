51e7084d41ae48ae2d1b05a4d776d20b492c78c6
Mathias Söderberg <mathias.soederberg@gmail.com>
2016-07-11 19:37:10 +0200

    More consistent creation (and usage) of helpers

--- test/integration/helpers/serverspec/spec_helper.rb
+++ test/integration/helpers/serverspec/spec_helper.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 require 'serverspec'
-require 'pathname'
 
 require 'support/platform_helpers'
 require 'support/await_helper'
@@ -10,6 +9,4 @@ RSpec.configure do |config|
   config.backend = :exec
   config.fail_fast = true
   config.order = :random
-  config.include(PlatformHelpers)
-  config.extend(PlatformHelpers)
 end
