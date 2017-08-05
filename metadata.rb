16471797b8dc333fd1736abe53ea074a81c98d2a
Tim Smith <tsmith@chef.io>
2017-08-05 16:53:29 -0700

    No need to check these metadata fields on Chef 12+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -18,5 +18,5 @@ supports 'ubuntu'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-source_url 'https://github.com/mthssdrbrg/kafka-cookbook' if respond_to?(:source_url)
-issues_url 'https://github.com/mthssdrbrg/kafka-cookbook/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/mthssdrbrg/kafka-cookbook'
+issues_url 'https://github.com/mthssdrbrg/kafka-cookbook/issues'
