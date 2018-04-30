1dd4257a988b84014e44d0812851c2417d0f3edf
Biju Nair <bnair10@bloomberg.net>
2018-04-30 14:55:31 -0400

    Create unique GC log files and set limits on the space usage

--- recipes/_defaults.rb
+++ recipes/_defaults.rb
@@ -5,7 +5,10 @@
 
 unless node['kafka']['gc_log_opts']
   node.default['kafka']['gc_log_opts'] = %W[
-    -Xloggc:#{::File.join(node['kafka']['log_dir'], 'kafka-gc.log')}
+    -Xloggc:#{::File.join(node['kafka']['log_dir'], 'kafka-gc-pid-$$-$(hostname)-$(date +\'%Y%m%d%H%M\').log')}
+    -XX:+UseGCLogFileRotation
+    -XX:NumberOfGCLogFiles=20
+    -XX:GCLogFileSize=20M
     -XX:+PrintGCDateStamps
     -XX:+PrintGCTimeStamps
   ].join(' ')
